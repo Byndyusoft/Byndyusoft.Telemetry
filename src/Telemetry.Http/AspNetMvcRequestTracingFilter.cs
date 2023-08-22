ada4e6305ca7597b84eab08938abe9ade4b747a4
rgaynetdinov <foxcoolman@byndyusoft.com>
2023-08-22 14:05:14 +0500

    Зачатки кода по телеметрии

---
+++ src/Telemetry.Http/AspNetMvcRequestTracingFilter.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Byndyusoft.MaskedSerialization.Newtonsoft.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Byndyusoft.AspNetCore.Mvc.Telemetry.Http
+{
+    public class AspNetMvcRequestTracingFilter : IAsyncActionFilter
+    {
+        private readonly AspNetMvcTracingOptions _options;
+
+        public AspNetMvcRequestTracingFilter(IOptions<AspNetMvcTracingOptions> options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            _options = options.Value;
+        }
+
+        public Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next)
+        {
+            return OnActionExecutionAsync(context, next, context.HttpContext.RequestAborted);
+        }
+
+        private async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next,
+            CancellationToken cancellationToken)
+        {
+            var tags = new TelemetryInfoItemCollection
+            {
+                { "http.request.header.accept", context.HttpContext.Request.Headers["accept"].ToArray() },
+                { "http.request.header.content_type", context.HttpContext.Request.ContentType },
+                { "http.request.header.content_length", context.HttpContext.Request.ContentLength }
+            };
+
+            foreach (var (name, value) in GetParameters(context))
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var json = await _options.FormatAsync(value, cancellationToken)
+                    .ConfigureAwait(false);
+                tags.Add($"http.request.params.{name}", json);
+            }
+
+            //var @event = new ActivityEvent("Action executing", tags: tags);
+            //activity.AddEvent(@event);
+
+            await next();
+        }
+
+        private static IEnumerable<(string name, object? value)> GetParameters(ActionExecutingContext context)
+        {
+            foreach (var actionParameter in context.ActionDescriptor.Parameters)
+            {
+                if (actionParameter.BindingInfo?.BindingSource == BindingSource.Services ||
+                    actionParameter.BindingInfo?.BindingSource == BindingSource.Special)
+                    continue;
+
+                var name = actionParameter.Name;
+                if (context.ActionArguments.TryGetValue(actionParameter.Name, out var value))
+                {
+                    yield return (name, value);
+                }
+            }
+        }
+    }
+
+    public class AspNetMvcTracingOptions
+    {
+        public const int DefaultValueMaxStringLength = 2000;
+
+        private IFormatter _formatter;
+        private int? _valueMaxStringLength;
+
+        public AspNetMvcTracingOptions()
+        {
+            _formatter = new NewtonsoftJsonFormatter();
+            _valueMaxStringLength = DefaultValueMaxStringLength;
+        }
+
+        public IFormatter Formatter
+        {
+            get => _formatter;
+            set => _formatter = value ?? throw new ArgumentNullException(nameof(Formatter));
+        }
+
+        public int? ValueMaxStringLength
+        {
+            get => _valueMaxStringLength;
+            set => _valueMaxStringLength = value ?? throw new ArgumentNullException(nameof(ValueMaxStringLength));
+        }
+
+        internal ValueTask<string?> FormatAsync(object? value, CancellationToken cancellationToken = default)
+        {
+            return Formatter.FormatAsync(value, this, cancellationToken);
+        }
+
+        internal void Configure(AspNetMvcTracingOptions options)
+        {
+            Formatter = options.Formatter;
+            ValueMaxStringLength = options.ValueMaxStringLength;
+        }
+    }
+
+    public interface IFormatter
+    {
+        ValueTask<string?> FormatAsync(object? value, AspNetMvcTracingOptions options,
+            CancellationToken cancellationToken);
+    }
+
+    public abstract class FormatterBase : IFormatter
+    {
+        public async ValueTask<string?> FormatAsync(object? value, AspNetMvcTracingOptions options,
+            CancellationToken cancellationToken = default)
+        {
+            if (value is null)
+                return "null";
+
+            using var stream = new StringLimitStream(options.ValueMaxStringLength);
+
+            await FormatValueAsync(value, stream, options, cancellationToken)
+                .ConfigureAwait(false);
+
+            return stream.GetString();
+        }
+
+        protected abstract ValueTask FormatValueAsync(object value, Stream stream, AspNetMvcTracingOptions options,
+            CancellationToken cancellationToken);
+    }
+
+    public class NewtonsoftJsonFormatter : FormatterBase
+    {
+        private JsonSerializerSettings _settings;
+
+        public NewtonsoftJsonFormatter()
+        {
+            _settings = new JsonSerializerSettings();
+            _settings.SetupSettingsForMaskedSerialization();
+        }
+
+        public JsonSerializerSettings Settings
+        {
+            get => _settings;
+            set => _settings = value ?? throw new ArgumentNullException(nameof(Settings));
+        }
+
+        protected override async ValueTask FormatValueAsync(
+            object value,
+            Stream stream,
+            AspNetMvcTracingOptions options,
+            CancellationToken cancellationToken)
+        {
+            var writer = new StreamWriter(stream, null!, -1, true);
+            using var jsonWriter = new JsonTextWriter(writer);
+
+            var serializer = JsonSerializer.Create(Settings);
+            serializer.Serialize(jsonWriter, value);
+
+            await jsonWriter.FlushAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    internal class StringLimitStream : Stream
+    {
+        private readonly int? _lengthLimit;
+        private MemoryStream? _memory;
+        private bool _oversized;
+
+        public StringLimitStream(int? lengthLimit)
+        {
+            _lengthLimit = lengthLimit;
+            _memory = new MemoryStream();
+        }
+
+        [ExcludeFromCodeCoverage] public override bool CanRead => Inner.CanRead;
+
+        [ExcludeFromCodeCoverage] public override bool CanSeek => Inner.CanSeek;
+
+        [ExcludeFromCodeCoverage] public override bool CanWrite => Inner.CanWrite;
+
+        [ExcludeFromCodeCoverage] public override long Length => Inner.Length;
+
+        [ExcludeFromCodeCoverage]
+        public override long Position
+        {
+            get => Inner.Position;
+            set => Inner.Position = value;
+        }
+
+        private MemoryStream Inner => _memory ?? throw new ObjectDisposedException(nameof(StringLimitStream));
+
+        [ExcludeFromCodeCoverage]
+        public override void Flush()
+        {
+            Inner.Flush();
+        }
+
+        [ExcludeFromCodeCoverage]
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return Inner.Read(buffer, offset, count);
+        }
+
+        [ExcludeFromCodeCoverage]
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return Inner.Seek(offset, origin);
+        }
+
+        [ExcludeFromCodeCoverage]
+        public override void SetLength(long value)
+        {
+            Inner.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            var inner = Inner;
+
+            if (_lengthLimit != null && inner.Length + count > _lengthLimit)
+            {
+                _oversized = true;
+                count = _lengthLimit.Value - (int)inner.Length;
+            }
+
+            Inner.Write(buffer, offset, count);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _memory?.Dispose();
+                _memory = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        public string GetString()
+        {
+            var str = Encoding.UTF8.GetString(Inner.ToArray());
+            return _oversized ? $"{str}..." : str;
+        }
+    }
+}
---
+++ src/Telemetry/TelemetryInfoItem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Byndyusoft.AspNetCore.Mvc.Telemetry
+{
+    public class TelemetryInfoItem
+    {
+        public TelemetryInfoItem(string key, object? value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; }
+
+        public object? Value { get; }
+    }
+
+    public class TelemetryInfoItemCollection : IEnumerable<TelemetryInfoItem>
+    {
+        private readonly List<TelemetryInfoItem> _telemetryInfoItems;
+
+        public TelemetryInfoItemCollection(params TelemetryInfoItem[] telemetryInfoItems)
+        {
+            _telemetryInfoItems = telemetryInfoItems.ToList();
+        }
+
+        public void Add(TelemetryInfoItem telemetryInfoItem)
+        {
+            _telemetryInfoItems.Add(telemetryInfoItem);
+        }
+
+        public void Add(string key, object? value)
+        {
+            var telemetryInfoItem = new TelemetryInfoItem(key, value);
+            Add(telemetryInfoItem);
+        }
+
+        public IEnumerator<TelemetryInfoItem> GetEnumerator()
+        {
+            return _telemetryInfoItems.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public static class TelemetryWriterNames
+    {
+        public static string Log => "Log";
+    }
+
+    public interface ITelemetryWriter
+    {
+        void Write(TelemetryInfoItemCollection telemetryInfoItemCollection);
+    }
+
+    public class LogWriter : ITelemetryWriter
+    {
+        private readonly ILogger<LogWriter> _logger;
+
+        public LogWriter(ILogger<LogWriter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Write(TelemetryInfoItemCollection telemetryInfoItemCollection)
+        {
+            var messageBuilder = new StringBuilder("Telemetry Message: ");
+            var arguments = new List<object>();
+
+            foreach (var telemetryInfoItem in telemetryInfoItemCollection)
+            {
+                messageBuilder.Append($" {telemetryInfoItem.Key} = {{{telemetryInfoItem.Key}}};");
+                arguments.Add(telemetryInfoItem.Value);
+            }
+
+            _logger.LogInformation(messageBuilder.ToString(), arguments.ToArray());
+        }
+    }
+}

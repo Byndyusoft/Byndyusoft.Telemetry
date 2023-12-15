5c1ab67f9f8e2642a23902ef224579f2cbdd5e34
rgaynetdinov <foxcoolman@byndyusoft.com>
2023-12-15 16:25:12 +0500

    Добавил атрибут для определения свойств, которые нужно логировать в качестве тэгов

---
+++ src/Telemetry.Abstraction/Attributes/LogAsTagAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Byndyusoft.AspNetCore.Mvc.Telemetry.Abstraction.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class LogAsTagAttribute : Attribute
+    {
+    }
+}

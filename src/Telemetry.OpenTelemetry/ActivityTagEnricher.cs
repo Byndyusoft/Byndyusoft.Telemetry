023c78bfb7959bd18d974b8fd91d46222192cf70
rgaynetdinov <foxcoolman@byndyusoft.com>
2023-12-20 12:11:36 +0500

    Добавил обогащатель тэгами данных телеметрии

---
+++ src/Telemetry.OpenTelemetry/ActivityTagEnricher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Byndyusoft.AspNetCore.Mvc.Telemetry.OpenTelemetry
+{
+    public class ActivityTagEnricher
+    {
+        public static void Enrich(IEnumerable<TelemetryItem> telemetryItems)
+        {
+            var activity = Activity.Current;
+            if (activity is null)
+                return;
+
+            foreach (var telemetryItem in telemetryItems) 
+                activity.AddTag(telemetryItem.Name, telemetryItem.Value);
+        }
+    }
+}
